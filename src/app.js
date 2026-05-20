a3a33004da7c90c20c0d23248d9829d19908a156
espejosara <espejo.sara93@gmail.com>
2026-05-20 21:23:54 +0200

    reorganizar carpetas

--- src/app.js
+++ src/app.js
@@ -6,13 +6,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// src/db/products.js
-export const products = [
-  { id: 1, name: "Teclado Mecánico", price: 100 },
-  { id: 2, name: "Monitor", price: 150 },
-  { id: 3, name: "Ratón Inalámbrico", price: 50 },
-  { id: 4, name: "Grafica", price: 30 },
-];
 
 
 export default app;
