552dc1c7d34e41a271eb120b1038b69d7cdfaed3
espejosara <espejo.sara93@gmail.com>
2026-05-20 21:28:54 +0200

    configurar rutas principales y agregar script dev

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import indexRoutes from './routes/index.routes.js';
+import productsRoutes from './routes/products.routes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
+app.use(indexRoutes);
+app.use(productsRoutes);
 
 export default app;
