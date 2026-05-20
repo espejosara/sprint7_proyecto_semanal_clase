de0ef55d0cd342288eaa3725c2ea7e0dc51d3228
espejosara <espejo.sara93@gmail.com>
2026-05-20 18:33:34 +0200

    middlewares de express y archivos de rutas base

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Aquí configuraremos las rutas más adelante
 
--- src/server.js
+++ src/server.js
@@ -5,5 +5,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-
-console.log("El servidor se está ejecutando");
