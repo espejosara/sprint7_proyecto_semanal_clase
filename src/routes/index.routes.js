4631bcd887a6d6758208b10dd68aecfb32034ac8
espejosara <espejo.sara93@gmail.com>
2026-05-28 18:19:53 +0200

    añadir endpoint DELETE y completar CRUD de productos

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,9 @@
-// src/routes/index.routes.js
 import { Router } from 'express';
 
 const router = Router();
 
 router.get('/health', (req, res) => {
-  res.status(200).json({ ok: true, data: { status: 'OK', message: 'Servicio funcional' } });
+  res.status(200).json({ ok: true, data: { message: 'Servidor funcionando correctamente' } });
 });
 
 export default router;
