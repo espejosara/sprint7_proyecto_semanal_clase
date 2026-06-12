cf2956dd709363f41243649f3f2b5e3283ea0765
espejosara <espejo.sara93@gmail.com>
2026-06-12 20:59:11 +0200

    añadir registro, login con JWT y seguridad web básica

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -22,3 +22,23 @@ export const register = async (req, res, next) => {
     next(error);
   }
 };
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ ok: false, error: 'Email y password son obligatorios' });
+    }
+
+    const token = await authService.loginUser(email, password);
+
+    res.status(200).json({
+      ok: true,
+      data: { token }
+    });
+  } catch (error) {
+    if (error.message === 'Credenciales inválidas') return res.status(401).json({ ok: false, error: error.message });
+    next(error);
+  }
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,4 +6,7 @@ const router = Router();
 // Ruta para registrar un usuario
 router.post('/api/auth/register', authController.register);
 
+// Ruta para iniciar sesión
+router.post('/api/auth/login', authController.login);
+
 export default router;
