ec8bd12271196b647762acc2d430ce3e97c8e84e
espejosara <espejo.sara93@gmail.com>
2026-06-15 17:51:53 +0200

    cambiar nombre de carpeta

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import prisma from '../db/prisma.js';
+import prisma from '../lib/prisma.js';
 
 export const registerUser = async (email, password) => {
   const existingUser = await prisma.user.findUnique({
