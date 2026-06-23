7f4a6e062649bb62d50118eb441993a7ab8d363a
espejosara <espejo.sara93@gmail.com>
2026-06-23 15:27:19 +0200

    quitar fileFilter de multer para compatibilidad con Postman

--- src/middlewares/uploadProductImage.js
+++ src/middlewares/uploadProductImage.js
@@ -4,12 +4,6 @@ import { storage } from '../config/cloudinary.js';
 const uploadProductImage = multer({
   storage,
   limits: { fileSize: 5 * 1024 * 1024 },
-  fileFilter: (req, file, cb) => {
-    if (!file.mimetype.startsWith('image/')) {
-      return cb(new Error('Solo se permiten archivos de imagen'));
-    }
-    cb(null, true);
-  },
 });
 
 export default uploadProductImage;
